e0c649b0c2ea120d28a04d19584c47e8f834a034
Phoenix616 <mail@moep.tv>
2018-09-06 20:53:17 +0100

    Change license to GPLv3 and include license file

--- src/main/java/de/minebench/plotgenerator/commands/BuyPlotCommand.java
+++ src/main/java/de/minebench/plotgenerator/commands/BuyPlotCommand.java
@@ -1,19 +1,21 @@
 package de.minebench.plotgenerator.commands;
 
 /*
- * Copyright 2016 Max Lee (https://github.com/Phoenix616/)
- * 
+ * PlotGenerator
+ * Copyright (c) 2018 Max Lee aka Phoenix616 (mail@moep.tv)
+ *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Mozilla Public License as published by
- * the Mozilla Foundation, version 2.
- * 
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Mozilla Public License v2.0 for more details.
- * 
- * You should have received a copy of the Mozilla Public License v2.0
- * along with this program. If not, see <http://mozilla.org/MPL/2.0/>.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 import com.sk89q.worldedit.Vector;
